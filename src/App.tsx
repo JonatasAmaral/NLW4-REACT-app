6c81fda54ac794f5c3bdbb58f83c01d25eed945a
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-23 00:26:29 -0300

    init react-app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ExperienceBar } from './components/ExperienceBar';
+import './styles/global.css';
+
+function App() {
+  return (
+    <div className='container'>
+    </div>
+  );
+}
+
+export default App;
