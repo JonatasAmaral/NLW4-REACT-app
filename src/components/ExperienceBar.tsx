b2545ba42ca6efbbf89d9b072b3c032326ea0b11
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-23 00:39:41 -0300

    tamanho barra de progresso dinamico (variavel)

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,19 +1,25 @@
 import {useState} from 'react';
 
-export function ExperienceBar(){    
+export function ExperienceBar(){
+    const [currentExperience, setExperience] = useState(0.4);
+    const [minExp, maxExp] = [0, 600];
+
+    const experiencePercent = ()=>currentExperience*100+'%'
+
+
     return(
         <div>
             <header className="experience-bar">
                 <span>0 xp</span>
                 <div>
-                    <div className="current-experience-bar" style={{width: '50%'}}/>
+                    <div className="current-experience-bar" style={{width: experiencePercent()}}/>
                     <span
                         className="current-experience-value"
                         style={{
-                            left: '50%'
+                            left: experiencePercent()
                         }}
                     >
-                        300 xp
+                        {Math.floor((maxExp-minExp)*currentExperience)} xp
                     </span>
                 </div>
                 <span>600 xp</span>
