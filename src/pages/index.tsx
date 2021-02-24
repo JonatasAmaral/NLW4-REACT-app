e885134794499712ce856a1e4926160ca3150545
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-24 14:12:13 -0300

    add config aos headers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Head from 'next/head'
+
 import styles from '../styles/pages/Home.module.css';
 
 import { ExperienceBar } from "../components/ExperienceBar";
@@ -10,6 +12,11 @@ import { Countdown } from "../components/Countdown";
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+      
       <ExperienceBar />
       
       <section>
