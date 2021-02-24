e390c4d4d509892a186d4fd8ef8621d9c5209c77
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-24 15:29:09 -0300

    valor de contador dinamico

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import styles from '../styles/components/Countdown.module.css';
 
 export function Countdown(){
+    const [time, setTime] = useState(25*60);
+
+    const minutes = Math.floor(time/60);
+    const seconds = time - minutes*60; // time % 60
+
+    const digitsMinutes = String(minutes).padStart(2, '0').split('');
+    const digitsSeconds = String(seconds).padStart(2, '0').split('');
+
+
     return(
         <div>
             <div className={styles.countdownContainer}>
                 <div>
-                    <span>2</span>
-                    <span>5</span>
+                    <span>{digitsMinutes[0] || 0}</span>
+                    <span>{digitsMinutes[1] || 0}</span>
                 </div>
                 <span>:</span>
                 <div>
-                    <span>0</span>
-                    <span>0</span>
+                    <span>{digitsSeconds[0] || 0}</span>
+                    <span>{digitsSeconds[1] || 0}</span>
                 </div>
             </div>
             <button type="button" className={styles.countdownButton} >
