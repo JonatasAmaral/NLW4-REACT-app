bb72edf41464d8a04b81ad8d6f58a443810f2398
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-26 01:32:31 -0300

    acoplar provider no contexto de challenges

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,3 +1,21 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
+import { render } from 'react-dom';
 
-export const ChallengesContext = createContext({});
+export const ChallengesContext = createContext(null);
+
+export function ChallengesProvider ({children}) {
+    const [level, setLevel] = useState(1)
+
+    function levelUp() {
+        setLevel(level + 1);
+    }
+
+    return (
+        <ChallengesContext.Provider value={{
+            level,
+            levelUp
+        }}>
+            {children}
+        </ChallengesContext.Provider>
+    )
+};
