df15b3ba8447f8dad57657686675bc828cf04512
Jonatas Amaral <jonataspadtab@gmail.com>
2021-02-28 20:09:51 -0300

    compartilhar levelUP apenas pela API WEB nativa

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -3,8 +3,17 @@ import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/LevelUpModal.module.css';
 
 export function LevelUpModal(){
-    const socialMediaName = 'twitter' // twitter, facebook, linkedin, whatsapp, discord
     const {level, closeLevelUpModal} = useContext(ChallengesContext);
+    let socialMediaName = '' // twitter, facebook, linkedin, whatsapp, discord
+    let canUseShareAPI = false;
+    
+    try{
+        if(navigator.share){
+            canUseShareAPI = true;
+        }
+    }
+    catch(err){}
+    
     return(
         
         <div className={styles.overlay}>
@@ -13,13 +22,23 @@ export function LevelUpModal(){
                 <strong>Parabéns</strong>
                 <p>Você alcançou um novo level</p>
 
-                <button className={styles.shareButton}>
-                    compartilhar{ socialMediaName? ' no ':'' }<span className="socialMedia">{socialMediaName}</span>
-                    <span className={`icon-${socialMediaName || 'share'}`}></span>
-                </button>
+                {canUseShareAPI && (
+                    <button
+                        className={styles.shareButton}
+                        onClick={()=>{
+                            navigator.share({
+                                title: `Avancei para o nivel ${level} no move.it!`,
+                                text: `Eu avancei para o nivel ${level} no move.it. Da só uma olhada!`,
+                                url: document.location.href
+                            })
+                        }}
+                    >
+                        compartilhar{ socialMediaName? ' no ':'' }<span className="socialMedia">{socialMediaName}</span>
+                        <span className={`icon-${socialMediaName || 'share'}`}></span>
+                    </button>
+                )}
 
                 <button type="button" className={styles.closeModal} onClick={closeLevelUpModal}>
-                    {/* <img src="/icons/close.svg" alt="Fechar modal"/> */}
                     <span className="icon-" aria-label="fechar modal de levelup">✖</span>
                 </button>
             </div>
